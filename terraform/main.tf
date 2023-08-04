11acfef06f87153ae0a906d0774b53328b48d356
Gokuldev-PS <132561683+Gokuldev-PS@users.noreply.github.com>
2023-08-04 16:01:52 +0530

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,166 @@
+
+
+
+#This part creates cluster inside environment
+resource "confluent_kafka_cluster" "basic" {
+  display_name = "Terraform_Dev"
+  availability = "SINGLE_ZONE"
+  cloud        = "AWS"
+  region       = "us-east-2"
+  basic {}
+
+  environment {
+    
+  #id=confluent_environment.development.id
+  id="env-2rr6r1"
+  }
+
+  
+}
+
+##This part creates service account
+
+resource "confluent_service_account" "terraform_dev" {
+  display_name = "terraform_dev"
+  description  = "terraform created"
+
+  
+}
+
+##This part assigned role to the user  account created
+resource "confluent_role_binding" "terraform_user-kafka-cluster-admin" {
+  principal   = "User:${confluent_service_account.terraform_dev.id}"
+  role_name   = "CloudClusterAdmin"
+  crn_pattern = confluent_kafka_cluster.basic.rbac_crn
+}
+
+# This part creates API Key for service account
+resource "confluent_api_key" "terraform_Created_APIKEY" {
+  display_name = "terraform_Created_APIKEY-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'terraform_dev' service account"
+  owner {
+    id          = confluent_service_account.terraform_dev.id
+    api_version = confluent_service_account.terraform_dev.api_version
+    kind        = confluent_service_account.terraform_dev.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.basic.id
+    api_version = confluent_kafka_cluster.basic.api_version
+    kind        = confluent_kafka_cluster.basic.kind
+
+    environment {
+    # id = confluent_environment.development.id
+       id="env-2rr6r1"
+    }
+  }
+    
+  
+  
+
+ 
+}
+   
+
+# This part creates a topic 
+
+resource "confluent_kafka_topic" "Fleet_Location" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "Fleet_Location"
+  rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+  partitions_count   = 2
+  credentials {
+    key   = confluent_api_key.terraform_Created_APIKEY.id
+    secret = confluent_api_key.terraform_Created_APIKEY.secret
+  }
+}
+resource "confluent_kafka_topic" "Fleet_User" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  topic_name    = "Fleet_User"
+  partitions_count   = 2
+  rest_endpoint      = confluent_kafka_cluster.basic.rest_endpoint
+  credentials {
+    key   = confluent_api_key.terraform_Created_APIKEY.id
+    secret = confluent_api_key.terraform_Created_APIKEY.secret
+  }
+}
+
+  
+resource "confluent_ksql_cluster" "example" {
+  display_name = "example"
+  csu          = 4
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  credential_identity {
+    id = confluent_service_account.terraform_dev.id
+  }
+  environment {
+    #id = confluent_environment.development.id
+    id="env-2rr6r1"
+     
+  }
+  
+
+
+  
+}
+
+#creating datagen source connector
+resource "confluent_connector" "datagen-source" {
+  environment {
+    #id = confluent_environment.development.id
+    id="env-2rr6r1"
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "Fleet_Location_Connector"
+    "kafka.auth.mode"          = "SERVICE_ACCOUNT"
+    "kafka.service.account.id" = confluent_service_account.terraform_dev.id
+    "kafka.topic"              = confluent_kafka_topic.Fleet_Location.topic_name
+    "output.data.format"       = "JSON"
+    "quickstart"               = "FLEET_MGMT_LOCATION"
+    "tasks.max"                = "1"
+  }
+
+
+  depends_on = [
+    confluent_api_key.terraform_Created_APIKEY,
+  ]
+}
+resource "confluent_connector" "datagen-source2" {
+  environment {
+    #id = confluent_environment.development.id
+    id="env-2rr6r1"
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.basic.id
+  }
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "Fleet_User_Connector"
+    "kafka.auth.mode"          = "SERVICE_ACCOUNT"
+    "kafka.service.account.id" = confluent_service_account.terraform_dev.id
+    "kafka.topic"              = confluent_kafka_topic.Fleet_User.topic_name
+    "output.data.format"       = "JSON"
+    "quickstart"               = "FLEET_MGMT_DESCRIPTION"
+    "tasks.max"                = "1"
+  }
+
+
+  depends_on = [
+    confluent_api_key.terraform_Created_APIKEY,
+  ]
+}
+
